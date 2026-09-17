f53b257a4bfd222bb0275cc07f40e41f14564300
OmarIsmail277 <omar.ismail277@gmail.com>
2026-09-17 15:19:38 +0300

    Refactor: Update reference paths in app.ts, project-input.ts, project-item.ts, and project-list.ts for improved module organization

--- Section13-Drag_Drop/src/components/project-list.ts
+++ Section13-Drag_Drop/src/components/project-list.ts
@@ -1,4 +1,8 @@
 /// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../state/project-state.ts" />
+/// <reference path="../models/project.ts" />
+/// <reference path="../models/drag-drop.ts" />
 
 namespace App {
   // besides, having our ProjetInput Class here, which is responsible for rendering the form and gathering the user input,
