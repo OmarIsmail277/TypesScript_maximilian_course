ade55c84bc4f6899d788c234a127f68c65179b33
OmarIsmail277 <omar.ismail277@gmail.com>
2026-09-17 12:04:13 +0300

    added Inheritance and generics

--- Section13-Drag_Drop/src/app.ts
+++ Section13-Drag_Drop/src/app.ts
@@ -15,14 +15,23 @@ class Project {
 }
 
 // Project State Management
-type Listener = (items: Project[]) => void;
+type Listener<T> = (items: T[]) => void;
 
-class ProjectState {
-  private listeners: Listener[] = [];
+class State<T> {
+  protected listeners: Listener<T>[] = [];
+
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
+  }
+}
+
+class ProjectState extends State<Project> {
   private projects: Project[] = [];
   private static instance: ProjectState;
 
-  private constructor() {}
+  private constructor() {
+    super();
+  }
 
   static getInstance() {
     if (this.instance) {
@@ -30,10 +39,6 @@ class ProjectState {
     }
     this.instance = new ProjectState();
     return this.instance;
-  }
-
-  addListener(listenerFn: Listener) {
-    this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
@@ -123,31 +128,62 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjustedDescriptor;
 }
 
-// besides, having our ProjetInput Class here, which is responsible for rendering the form and gathering the user input,
-// we want to have another class which is responsible for rendering a list of projects => splitted into 2 classes, => one for the list,
-// one class per project item in the list
-
-class ProjectList {
+// Component Base Class
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLElement;
-  assignedProjects: Project[];
+  hostElement: T;
+  element: U;
 
-  constructor(private type: "active" | "finished") {
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string,
+  ) {
     this.templateElement = document.getElementById(
-      "project-list",
+      templateId,
     )! as HTMLTemplateElement;
 
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
-    this.assignedProjects = [];
+    this.hostElement = document.getElementById(hostElementId)! as T;
 
     const importedNode = document.importNode(
       this.templateElement.content,
       true,
     );
-    this.element = importedNode.firstElementChild as HTMLElement;
-    this.element.id = `${this.type}-projects`;
+    this.element = importedNode.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+    this.attach(insertAtStart);
+  }
 
+  private attach(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBeginning ? "afterbegin" : "beforeend",
+      this.element,
+    );
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
+}
+
+// besides, having our ProjetInput Class here, which is responsible for rendering the form and gathering the user input,
+// we want to have another class which is responsible for rendering a list of projects => splitted into 2 classes, => one for the list,
+// one class per project item in the list
+
+class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+  assignedProjects: Project[];
+
+  constructor(private type: "active" | "finished") {
+    super("project-list", "app", false, `${type}-projects`);
+    this.assignedProjects = [];
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {
     projectState.addListener((projects: Project[]) => {
       const relevantProjects = projects.filter((prj) => {
         if (this.type === "active") {
@@ -158,9 +194,13 @@ class ProjectList {
       this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
+  }
 
-    this.attach();
-    this.renderContent();
+  renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
   }
 
   private renderProjects() {
@@ -174,45 +214,16 @@ class ProjectList {
       listEl?.appendChild(listItem);
     }
   }
-
-  private renderContent() {
-    const listId = `${this.type}-projects-list`;
-    this.element.querySelector("ul")!.id = listId;
-    this.element.querySelector("h2")!.textContent =
-      this.type.toUpperCase() + " PROJECTS";
-  }
-
-  private attach() {
-    this.hostElement.insertAdjacentElement("beforeend", this.element);
-  }
 }
 
-class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   // inputs
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
   peopleInputElement: HTMLInputElement;
 
   constructor() {
-    // selection logic and rough-setup
-    // the blueprint
-    this.templateElement = document.getElementById(
-      "project-input",
-    )! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
-
-    // we need to import the content of that template element & render this to the dom
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true,
-    );
-    // The actual(concrete) form
-    this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = "user-input";
-
+    super("project-input", "app", true, "user-input");
     // get access to different inputs in that form
     this.titleInputElement = this.element.querySelector(
       "#title",
@@ -229,8 +240,16 @@ class ProjectInput {
     // in general to keep [selection and rough set-up in the constructor] & then [the insertion & fine-tuning in seperate methods]
     // delegate the work
     this.configure();
-    this.attach();
   }
+
+  // add a listener to our form - Sets up behavior
+  configure() {
+    // this.element.addEventListener("submit", this.submitHandler); // problem- this bound to the current target of the event (which is the form)
+    // it will not point to the class => solution => bind, better solution is with decorators(autobind)
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  renderContent() {}
 
   private gatherUserInput(): [string, string, number] | void {
     const enteredTitle = this.titleInputElement.value;
@@ -284,18 +303,6 @@ class ProjectInput {
       projectState?.addProject(title, desc, people);
       this.clearInputs();
     }
-  }
-
-  // add a listener to our form - Sets up behavior
-  private configure() {
-    // this.element.addEventListener("submit", this.submitHandler); // problem- this bound to the current target of the event (which is the form)
-    // it will not point to the class => solution => bind, better solution is with decorators(autobind)
-    this.element.addEventListener("submit", this.submitHandler);
-  }
-
-  // rendering logic - Actually puts the form into the DOM
-  private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
