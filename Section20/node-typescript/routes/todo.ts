1e5443fc8da1522ef54ba0c7c72e277e57e2f67f
OmarIsmail277 <omar.ismail277@gmail.com>
2026-09-20 14:34:59 +0300

    Enhance todo routes with CRUD operations and error handling

--- Section20/node-typescript/routes/todo.ts
+++ Section20/node-typescript/routes/todo.ts
@@ -1,14 +1,8 @@
 import express from "express";
-import { addTodo, getTodos } from "../data.js";
+import { addTodo, getTodos, getTodo, updateTodo, removeTodo } from "../data.js";
 
 // simplify the process of defining routes that can be handled by Express
 const router = express.Router();
-
-router.get("/todos", (req, res) => {
-  const todos = getTodos();
-
-  res.json({ todos });
-});
 
 router.post("/todos", (req, res) => {
   const text = req.body.text;
@@ -16,6 +10,35 @@ router.post("/todos", (req, res) => {
   const addedTodo = addTodo(text);
 
   res.json({ message: "Todo Added!", todo: addedTodo });
+});
+
+router.get("/todos", (req, res) => {
+  const todos = getTodos();
+
+  res.json({ todos });
+});
+
+router.get("/todos/:id", (req, res) => {
+  try {
+    const todo = getTodo(+req.params.id);
+    res.json({ todo });
+  } catch (error) {
+    res.status(404).json({ message: "Todo not found" });
+  }
+});
+
+router.patch("/todos/:id", (req, res) => {
+  try {
+    const updatedTodo = updateTodo(+req.params.id, req.body.text);
+    res.json({ message: "Todo updated", todo: updatedTodo });
+  } catch (error) {
+    res.status(404).json({ message: "Todo not found" });
+  }
+});
+
+router.delete("/todos/:id", (req, res) => {
+  removeTodo(+req.params.id);
+  res.json({ message: "Todo Deleted" });
 });
 
 export default router;
